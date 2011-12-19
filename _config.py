0050a53e2f29f3468815026d02e854787efbd207
tim <tdixon51793@gmail.com>
2011-12-18 20:36:24 -0600

    Use the new Interpreter decorator for the markdown interpreter.

--- _config.py
+++ _config.py
@@ -4,11 +4,11 @@ from cytoplasm.defaults import *
 # add a markdown interpreter
 import markdown
 markdown_extensions = ['abbr', 'footnotes', 'toc', 'fenced_code', 'headerid']
+
+@Interpreter("markdown", "md")
 def markdown_interpreter(file, destination):
     markdown.markdownFromFile(input=file, output=destination,
             extensions = markdown_extensions, encoding="utf8", safe=False)
-
-interpreters["markdown"] = markdown_interpreter
 
 # the first thing in the tuple is the name of the module where the controller lives;
 # the second thing is a list of arguments to be used when instantiating a controller object.
